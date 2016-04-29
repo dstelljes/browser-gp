4f3ba744e625cb8b89ebc9fa52ebb57f1bb7b698
Dan Stelljes <stell124@morris.umn.edu>
2016-04-29 12:28:30 -0500

    Completed crossover

--- library/operators/crossover.js
+++ library/operators/crossover.js
@@ -16,19 +16,41 @@
 
 'use strict';
 
+var splice = Array.prototype.splice;
+var program = require('../program');
+
 /**
  * Creates a crossover operator.
  *
  * @function
  * @memberof operators
  *
- * @param {PRNG} prng
+ * @param {PRNG} random
  * The random number generator to use for selecting crossover points.
  *
  * @returns {operators~CrossoverOperator}
  */
-var crossover = module.exports = function(prng) {
+var crossover = module.exports = function(random) {
   return function(a, b) {
-    
+    // Get crossover points:
+    var ax = random.integer(0, a.length);
+    var bx = random.integer(0, b.length);
+
+    // Quick and easy way to clone an array:
+    // @see https://davidwalsh.name/javascript-clone-array
+    var child = a.slice(0);
+
+    // Get the replacement subtree from b:
+    var args = program.extractSubtree(b, bx);
+
+    // To build the arguments for splice, we're going to prepend the start and
+    // delete count arguments to the subtree array:
+    // @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+    args.unshift(ax, program.findSubtreeLength(a, ax));
+
+    // Insert the replacement subtree on a:
+    splice.apply(child, args);
+
+    return child;
   };
 };
--- tests/library/operators/crossover.spec.js
+++ tests/library/operators/crossover.spec.js
@@ -13,7 +13,7 @@ var mul = gp.functions.multiplication;
 var div = gp.functions.division;
 
 describe('crossover', function() {
-  it('performs a crossover on two trees', function() {
+  it('replaces subtrees', function() {
     // (* 2 (/ 8 4))
     var even = [mul, 2, div, 8, 4];
 
@@ -25,11 +25,26 @@ describe('crossover', function() {
     // generator converts doubles to integers:
     // @see PRNG#integer
     var wheel = gp.random.wheel([0.4, 0.2]);
-    expect(wheel.integer(0, 5)).toBe(2);
-    expect(wheel.integer(0, 5)).toBe(1);
-    // Wheel has now output all randoms in its sequence & is back at the beginning.
+    expect(wheel.integer(0, even.length)).toBe(2);
+    expect(wheel.integer(0, odd.length)).toBe(1);
+    // And we only gave the wheel two values, so it's back at the beginning.
 
     var crossover = gp.operators.crossover(wheel);
     expect(crossover(even, odd)).toEqual([mul, 2, sub, 3, 6]);
+  });
+
+  it('replaces an entire tree', function() {
+    // (+ (- 0 8) 6)
+    var even = [add, sub, 0, 8, 6];
+
+    // (/ 9 3)
+    var odd = [div, 9, 3];
+
+    var wheel = gp.random.wheel([0.6, 0]);
+    expect(wheel.integer(0, even.length)).toBe(3);
+    expect(wheel.integer(0, odd.length)).toBe(0);
+
+    var crossover = gp.operators.crossover(wheel);
+    expect(crossover(even, odd)).toEqual([add, sub, 0, div, 9, 3, 6]);
   });
 });
