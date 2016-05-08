efd3c8d390bf6b6ded1ad1b4c440baeb823c75eb
Dan Stelljes <stell124@morris.umn.edu>
2016-05-08 15:04:07 -0500

    Wrote background worker

--- frontend/modules/dashboard/controllers/problem.run.js
+++ frontend/modules/dashboard/controllers/problem.run.js
@@ -3,6 +3,10 @@
 module.exports = function($scope, problem, run) {
   $scope.problem = problem;
   $scope.run = run;
+  $scope.worker = {
+    instance: null,
+    running: false
+  };
 
   $scope.functions = [{
     bundled: true,
@@ -24,5 +28,36 @@ module.exports = function($scope, problem, run) {
     category: 'Integers',
     key: 'division',
     name: 'Division'
+  }, {
+    bundled: true,
+    category: 'Integers',
+    key: 'protectedDivision',
+    name: 'Protected division'
   }];
+
+  $scope.evolve = function() {
+    var worker = $scope.worker.instance = new Worker('workers/run.js');
+
+    worker.postMessage({
+      action: 'run',
+      problem: $scope.problem,
+      run: $scope.run
+    });
+
+    worker.onmessage = function(message) {
+      switch (message.data.event) {
+        case 'evolved':
+          console.log('Generation ' + message.data.generation + ' had best score ' + message.data.best);
+          break;
+
+        case 'finished':
+          $scope.worker.running = false;
+          break;
+
+        case 'started':
+          $scope.worker.running = true;
+          break;
+      }
+    };
+  };
 };
